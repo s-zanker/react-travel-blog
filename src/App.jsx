fd26c625ff16f399c9e280da9a86452b17388158
Codingheart <codingheart@web.de>
2025-06-02 19:01:35 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+export function App() {
+  return (
+    <div className="App">
+      <h1 className="App-headline">Vite React Minimal Template</h1>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test.
+      </p>
+    </div>
+  );
+}
