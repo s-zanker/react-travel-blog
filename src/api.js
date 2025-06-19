c9ec17cb5ccfe16462d90a0176c2a47a110e855d
Codingheart <codingheart@web.de>
2025-06-19 14:49:48 +0200

    Add POST and DELETE requests to posts.http for API testing

--- src/api.js
+++ src/api.js
@@ -14,6 +14,32 @@ export async function fetchPostById(id) {
   const post = await response.json();
   return post; //returns the found post
 }
+
+export async function sendAddPost(newData) {
+  const response = await fetch(api, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newData),
+  });
+  const newId = await response.json();
+  return newId;
+}
+
+export async function sendPostUpdate(id, dataToUpdate) {
+  const uri = `${api}/${id}`;
+  const response = await fetch(uri, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dataToUpdate),
+  });
+  const updated = await response.json();
+  return updated; //true or false
+}
+
 //Authors
 export async function fetchAllAuthors() {
   const response = await fetch(api2);
